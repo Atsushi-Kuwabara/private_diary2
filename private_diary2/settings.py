a96f0314e9e2cbbde31f7198dddd6538c6fa0509
Atsushi-Kuwabara <48851734+Atsushi-Kuwabara@users.noreply.github.com>
2020-05-29 12:48:46 +0900

    DBをPSQLに変更

--- private_diary2/settings.py
+++ private_diary2/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'private_diary2.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'private_diary',
+        'USER': os.environ.get('PSQL_DB_USER'),
+        'PASSWORD': os.environ.get('PSQL_DB_PASSWORD'),
+        'HOST': '',
+        'PORT': '',
     }
 }
 
